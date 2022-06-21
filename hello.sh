fa2b111fd01a8f1413e574a7e1a062bc04249bea
root <root@ip-172-31-12-233.ap-south-1.compute.internal>
2022-06-21 09:08:44 +0000

    updated

---
+++ hello.sh
@@ -0,0 +1,8 @@
+echo "hello guys"
+#set -x
+echo "iam sandeep"
+echo "iam from cbpur"
+echo "i stay in bangalore"
+#set +x
+echo "i work in aatmaniaa solutions"
+
---
+++ udv.sh
@@ -0,0 +1,5 @@
+echo "enter two numbers"
+read a 
+read b
+sum=`expr $a + $b`
+echo $sum
